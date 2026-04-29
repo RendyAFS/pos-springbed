bc857bb010d1d0f7647101e5aa84be4a0a6cb61d
IMCPRendy <imcpengwebdev@gmail.com>
2026-04-29 10:23:30 +0700

    going to add services Referal

---
+++ database/migrations/2026_04_29_083004_create_referals_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('referals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->nullable()->constrained('customers')->cascadeOnDelete();
+            $table->string('name_customer')->nullable();
+            $table->string('referal_code')->nullable()->unique();
+            $table->integer('referal_amount')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->boolean('is_referal')->default(false)->after('transaction_date');
+            $table->foreignId('referal_customer_id')->nullable()->after('is_referal')->constrained('customers')->cascadeOnDelete();
+            $table->integer('nominal_referal')->nullable()->after('referal_customer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('referals');
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('is_referal');
+            $table->dropForeign(['referal_customer_id']);
+            $table->dropColumn('referal_customer_id');
+            $table->dropColumn('nominal_referal');
+        });
+    }
+};
