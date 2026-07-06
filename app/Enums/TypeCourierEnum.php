ca49efa91e791df822b84c00d6596c710be4c5c7
RendyAFS <arendy724@gmail.com>
2026-07-07 03:55:58 +0700

    feat: change list type courier

--- app/Enums/TypeCourierEnum.php
+++ app/Enums/TypeCourierEnum.php
@@ -4,14 +4,22 @@ namespace App\Enums;
 
 enum TypeCourierEnum: string
 {
-    case INTERNAL = 'internal';
-    case EXTERNAL = 'external';
+    case INTERNAL        = 'internal';
+    case EXTERNAL        = 'external';
+    case KURIR_TOKO      = 'kurir_toko';
+    case KURIR_LUAR_KOTA = 'kurir_luar_kota';
+    case EKSPEDISI       = 'ekspedisi';
+    case AMBIL_DITEMPAT  = 'ambil_ditempat';
 
     public function getLabel(): string
     {
         return match ($this) {
-            self::INTERNAL => 'Internal',
-            self::EXTERNAL => 'External',
+            self::INTERNAL        => 'Internal',
+            self::EXTERNAL        => 'External',
+            self::KURIR_TOKO      => 'Kurir Toko',
+            self::KURIR_LUAR_KOTA => 'Kurir Luar Kota',
+            self::EKSPEDISI       => 'Ekspedisi',
+            self::AMBIL_DITEMPAT  => 'Ambil Ditempat',
         };
     }
 }
